d4db7e67134034118e646e8d478de05d8905c20d
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-04-13 08:16:42 -0400

    Update startup.py
    Save space, security, change  to Englishmania!, etc

--- startup.py
+++ startup.py
@@ -1,18 +1,18 @@
-print("Welcome to Anglemania!")
-print()
+import runpy
+from sys import exit
+print("Welcome to Anglemania!\n")
 def load():
    guicli = raw_input('''Would you like a graphical interface or a text interface? (gui/cli, case-sensitive)
    Type:''')
    if guicli == "gui":
       print("Loading...")
       try:
-          execfile('python gui.py')
+          runpy.run_path(path_name="gui.py")
       except KeyboardInterrupt:
-          print("You exited the program.")
-          exit()
+          exit("You exited the program.")
       except IOError:
           print("Error in finding files, do they exist?")
-          print("(By default the terminal is in your home folder ~. Try cd'ing to the dir that Anglemania is in.)")
+          print("(By default the terminal is in your home folder ~. Try cd'ing to the dir that Englishmania! is in.)")
       except:
           print("An unrecognized error occured.")
    elif guicli == "cli":
@@ -24,4 +24,4 @@ def load():
       print("Please type either gui or cli, case-sensitive (gui is graphical, cli is text")
       load()
 load()
-print("Thanks for using Anglemania!")
+print("Thanks for using Englishmania!!")
